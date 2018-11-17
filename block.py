0fb5456f8022a41c0a76f6f88882bb63da031cbd
z2care <z2care@gmail.com>
2018-11-17 20:49:20 +0800

    add ability of sync with peer

--- block.py
+++ block.py
@@ -3,21 +3,47 @@ import hashlib
 import json
 import os
 
+from config import *
+
 class Block(object):
     def __init__(self, dictionary):
         '''
             We're looking for index, timestamp, data, prev_hash, nonce
         '''
-        for k, v in dictionary.items():
-            setattr(self, k, v)
+        for key, value in dictionary.items():
+            if key in BLOCK_VAR_CONVERSIONS:
+                setattr(self, key, BLOCK_VAR_CONVERSIONS[key](value))
+            else:
+                setattr(self, key, value)
 
         if not hasattr(self, 'nonce'):
             #we're throwin this in for generation
             self.nonce = 'None'
         if not hasattr(self, 'hash'): #in creating the first block, needs to be removed in future
-            self.hash = self.create_self_hash()
+            self.hash = self.update_self_hash()
 
-    def __dict__(self):
+    def header_string(self):
+        return str(self.index) + self.prev_hash + self.data + str(self.timestamp) + str(self.nonce)
+    def generate_header(self):
+        return str(index) + prev_hash + data + str(timestamp) + str(nonce)
+
+    def update_self_hash(self):
+        sha = hashlib.sha256()
+        sha.update(self.header_string())
+        new_hash = sha.hexdigest()
+        self.hash = new_hash
+        return new_hash
+
+    def self_save(self):
+        '''
+            Want the ability to easily save
+        '''
+        index_string = str(self.index).zfill(6) #front of zeros so they stay in numerical order
+        filename = '%s%s.json' % (CHAINDATA_DIR, index_string)
+        with open(filename, 'w') as block_file:
+            json.dump(self.to_dict(), block_file)
+
+    def to_dict(self):
         info = {}
         info['index'] = str(self.index)
         info['timestamp'] = str(self.timestamp)
@@ -25,45 +51,35 @@ class Block(object):
         info['hash'] = str(self.hash)
         info['data'] = str(self.data)
         info['nonce'] = str(self.nonce)
-        return info
 
-    def __str__(self):
-        return "Block<prev_hash: %s,hash: %s>" % (self.prev_hash, self.hash)
+    def is_valid(self):
+        '''
+            Current validity is only that the hash begins with at least NUM_ZEROS
+        '''
+        self.update_self_hash()
+        if str(self.hash[0:NUM_ZEROS]) == '0' * NUM_ZEROS:
+            return True
+        else:
+            return False
 
-    def header_string(self):
-        return str(self.index) + self.prev_hash + self.data + str(self.timestamp) + str(self.nonce)
+    def __repr__(self): #used for debugging without print, __repr__ > __str__
+        return "Block<index: %s>, <hash: %s>" % (self.index, self.hash)
 
-    def create_self_hash(self):
-        sha = hashlib.sha256()
-        sha.update(self.header_string())
-        return sha.hexdigest()
+    def __eq__(self, other):
+        return (self.index == other.index and
+            self.timestamp == other.timestamp and
+            self.prev_hash == other.prev_hash and
+            self.hash == other.hash and
+            self.data == other.data and
+            self.nonce == other.nonce)
 
-    def self_save(self):
-        chaindata_dir = 'chaindata'
-        index_string = str(self.index).zfill(6) #front of zeros so they stay in numerical order
-        filename = '%s/%s.json' % (chaindata_dir, index_string)
-        with open(filename, 'w') as block_file:
-            json.dump(self.__dict__(), block_file)
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        return self.timestamp < other.timestamp
+
+    def __lt__(self, other):
+        return self.timestamp > other.timestamp
 
 
-def create_first_block():
-    # index zero and arbitrary previous hash
-    block_data = {}
-    block_data['index'] = 0
-    block_data['timestamp'] = date.datetime.now()
-    block_data['data'] = 'First block data'
-    block_data['prev_hash'] = ''
-    block_data['nonce'] = 0 #starting it at 0
-    block = Block(block_data)
-    return block
-
-#check if chaindata folder exists.
-chaindata_dir = 'chaindata'
-if not os.path.exists(chaindata_dir):
-    #make chaindata dir
-    os.mkdir(chaindata_dir)
-    #check if dir is empty from just creation, or empty before
-if os.listdir(chaindata_dir) == []:
-    #create first block
-    first_block = create_first_block()
-    first_block.self_save()
